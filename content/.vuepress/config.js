ad5b4e063cb1eb072cd05981b8aa926bc272d78b
Zhang Junhua <zhangjh@act.buaa.edu.cn>
2021-06-21 10:47:04 +0800

    修正联系 github 地址

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -58,7 +58,7 @@ module.exports = {
         "items": [
           {
             "text": "GitHub",
-            "link": "https://github.com/recoluan",
+            "link": "https://github.com/SincereXIA",
             "icon": "reco-github"
           }
         ]
